5fe5752f86cb7e9c9afe762b099a18b98e0f77d0
BLadner34 <brittnee.hallman@yahoo.com>
2026-02-21 10:42:37 -0600

    added const tags variable and for loop

---
+++ app.js
@@ -0,0 +1,10 @@
+const tags = ["js", "week3"];
+const scores = [10, 25, 18, 30]; 
+
+let totalScore = 0;
+
+for (let index = 0; index < scores.length; index++) {
+    totalScore += scores[index];
+    
+}
+console.log(totalScore); 
